d4518fc692b0ae5ceba2489554882c9c5d16fd14
hyoshie_vm <dymn9.term@gmail.com>
2021-10-28 19:58:19 +0900

    50minutes

---
+++ guacamore/install.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
+brew install readline
+brew install bat
+brew install exa
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+cd  ~/.vim
+mkdir colors
+git clone https://github.com/tomasr/molokai
+mv molokai/colors/molokai.vim ~/.vim/colors/
