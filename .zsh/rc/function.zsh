fcc15763faeda86c45e888ec35c94b4dd605c6b8
Hiroki Yoshie <dymn9.term@gmail.com>
2022-01-17 01:39:38 +0900

    wip:split zshrc

---
+++ .zsh/rc/function.zsh
@@ -0,0 +1,68 @@
+# Function
+function cvim() {
+	command nvim $@ && clear
+}
+
+function gre() {
+	command grep --color=auto -E $1 -r1 $2
+}
+
+function mkcd() {
+	command mkdir -pv $1 && cd $1
+}
+
+function myleak() {
+	while [ 1 ]
+	do
+		leaks $1
+		sleep 1
+	done
+}
+
+function msleak() {
+	while [ 1 ]
+	do
+		leaks minishell
+		sleep 1
+	done
+}
+
+function msproc() {
+	while [ 1 ]
+	do
+		pgrep -l minishell
+		sleep 1
+	done
+}
+
+function mskill() {
+	pkill minishell
+}
+
+
+function grepc() {
+	LANG=C grep --color -r -n\
+		--exclude-dir='.git' \
+		--exclude-dir='.svn' \
+		--include='*.h' \
+		--include='*.hpp' \
+		--include='*.m' \
+		--include='*.c' \
+		--include='*.cc' \
+		--include='*.cpp' \
+		"$1" .
+}
+
+function greph() {
+	LANG=C grep --color -r -n\
+		--exclude-dir='.git' \
+		--exclude-dir='.svn' \
+		--include='*.h' \
+		--include='*.hpp' \
+		"$1" .
+}
+
+function pynt() {
+	python -c "print('$1' * $2)"
+}
+
---
+++ .zsh/rc/plugin.zsh
@@ -0,0 +1,9 @@
+# zsh-highlight
+source /Users/hyoshie/.brew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+typeset -A ZSH_HIGHLIGHT_STYLES
+ZSH_HIGHLIGHTING_HIGHLIGHTERS=(main brackets cursor)
+ZSH_HIGHLIGHT_STYLES[path]='underline'
+
+# zsh-autosuggestion
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
