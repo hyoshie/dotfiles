aa4d34c84d5c00c06f5570c7549c24faa9f41bd5
hyoshie_vm <dymn9.term@gmail.com>
2022-02-22 00:56:06 +0900

    chore:brewconfig.zsh

--- .brewconfig.zsh
+++ .brewconfig.zsh
@@ -3,16 +3,20 @@
 # Path
 if [ $(uname) = "Darwin" ]; then
 	export PATH=$HOME/.brew/bin:$PATH
+	export HOMEBREW=$HOME/.brew
 fi
-if [ $(uname) = "Linux" ]; then
+if [ $(uname)="Linux" ]; then
+	# export HOGE=hoge42tokyo
 	export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+	export HOMEBREW=/home/linuxbrew/.linuxbrew
 fi
-
 # Set Homebrew temporary folders
 # export HOMEBREW_CACHE=/tmp/$USER/Homebrew/Caches
 # export HOMEBREW_TEMP=/tmp/$USER/Homebrew/Temp
 export HOMEBREW_CACHE=$HOME/.cache/Homebrew/Caches
 export HOMEBREW_TEMP=$HOME/.cache/Homebrew/Temp
+
+export HOGE=hoge42tokyo
 
 mkdir -p $HOMEBREW_CACHE
 mkdir -p $HOMEBREW_TEMP
