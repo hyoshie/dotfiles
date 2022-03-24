7f0a95cc432e7f243204fe0986884869a5adc0f9
hyoshie_vm <dymn9.term@gmail.com>
2022-03-24 17:22:39 +0900

    chore: cub3d_settings

--- .zsh/rc/includepath.zsh
+++ .zsh/rc/includepath.zsh
@@ -1,5 +1,5 @@
 # export CPATH=$HOME/dotfiles/.include
-export CPATH=$HOME/dotfiles/.include:$HOME/42tokyo/cub3D/inc:$HOME/42tokyo/cub3D/lib/mililibx-linux
+export CPATH=$HOME/dotfiles/.include:$HOME/42tokyo/cub3D/inc:$HOME/42tokyo/cub3D/bonus/inc:$HOME/42tokyo/cub3D/lib/mililibx-linux
 # export C_INCLUDE_PATH=$HOME/dotfiles/.include:$HOME/42tokyo/cub_cooperate/inc:$HOME/42tokyo/cub_cooperate/lib/mililibx-linux
 # export C_INCLUDE_PATH=$HOME/dotfiles/.include
 # export C_INCLUDE_PATH=$HOME/dotfiles/.include:$HOME/42tokyo/cub3d/minimap/work/inc
--- .zsh/rc/prompt.zsh
+++ .zsh/rc/prompt.zsh
@@ -26,5 +26,5 @@ GIT_PS1_SHOWUPSTREAM=auto
 # プロンプトの表示設定(好きなようにカスタマイズ可)
 # setopt PROMPT_SUBST ; PS1='%F{green}%n%f[%?]: %F{cyan}%~%f %F{red}$(__git_ps1 "(%s)")%f
 # \$ '
-setopt PROMPT_SUBST ; PS1='%F{cyan}%~%f[%?]: %F{yellow}$(__git_ps1 "(%s)")%f
+setopt PROMPT_SUBST ; PS1='%F{cyan}%~%f[%?] %F{yellow}$(__git_ps1 "(%s)")%f
 \$ '
